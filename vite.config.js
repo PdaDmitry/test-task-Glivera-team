32d4f2b77dad763406f97f9867d3d49add652b4b
PdaDmitry <pda.gazcomp@gmail.com>
2024-11-08 15:15:21 +0200

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,45 @@
+// import { defineConfig } from 'vite';
+// import glob from 'glob';
+// import injectHTML from 'vite-plugin-html-inject';
+// import FullReload from 'vite-plugin-full-reload';
+
+// export default defineConfig(({ command }) => {
+//   return {
+//     define: {
+//       [command === 'serve' ? 'global' : '_global']: {},
+//     },
+//     root: 'src',
+//     build: {
+//       sourcemap: true,
+
+//       rollupOptions: {
+//         input: glob.sync('./src/*.html'),
+//         output: {
+//           manualChunks(id) {
+//             if (id.includes('node_modules')) {
+//               return 'vendor';
+//             }
+//           },
+//           entryFileNames: 'commonHelpers.js',
+//         },
+//       },
+//       outDir: '../dist',
+//     },
+//     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+//   };
+// });
+
 import { defineConfig } from 'vite';
-import glob from 'glob';
-import injectHTML from 'vite-plugin-html-inject';
-import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
+export default defineConfig({
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+    keepNames: true,
+  },
+  build: {
+    rollupOptions: {
+      external: ['axios'],
     },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
-      },
-      outDir: '../dist',
-    },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
+  },
 });
